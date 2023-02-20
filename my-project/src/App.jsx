fc746422c7fcc4fbed25fbe8dcd6717111c1fd5b
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-02-20 15:09:36 +0500

    btn add

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/buttons.css'
+
+function App() {
+  return (
+    <>
+      <button className='btn'>Click</button>
+      <button className='btn-red'>Click</button>
+    </>
+  );
+}
+
+export default App;
